600403708655dfb78181d5f628f34fbdd06a3d59
User <user@mail.net>
2024-01-26 15:59:24 +0200

    Sqrt(x) Solution

---
+++ Leet Code (learning)/PlusOne.swift
@@ -0,0 +1,33 @@
+//
+//  PlusOne.swift
+//  Leet Code (learning)
+//
+//  Created by Server Admin on 26.01.2024.
+//
+
+//import Foundation
+//
+//class Solution {
+//    func plusOne(_ digits: [Int]) -> [Int] {
+//
+//        var arr = digits
+//        var i = 1
+//        while arr[arr.count-i] == 9 {
+//            arr[arr.count-i] = 0
+//            if arr.count-i == 0 { arr.insert(0, at: 0)}
+//            i += 1
+//        }
+//        arr[arr.count-i] += 1
+//        return arr
+//    }
+//}
+//
+//
+//var digits = [9, 9, 9, 9]
+//var someSol = Solution()
+//
+//print("\(someSol.plusOne(digits))")
+//
+//
+//
+//pause()
---
+++ Leet Code (learning)/Sqrt(x).swift
@@ -0,0 +1,25 @@
+//
+//  Sqrt(x).swift
+//  Leet Code (learning)
+//
+//  Created by Server Admin on 26.01.2024.
+//
+
+//import Foundation
+//
+//    func mySqrt(_ x: Int) -> Int {
+//        return Int(Double(x).squareRoot())
+//    }
+//}
+//
+//class Solution {
+//    func mySqrt(_ x: Int) -> Int {
+//        return Int(sqrt(Double(x)))
+//    }
+//}
+//
+//
+//
+//var x = 8
+//var someSol = Solution()
+//print(someSol.mySqrt(x))
